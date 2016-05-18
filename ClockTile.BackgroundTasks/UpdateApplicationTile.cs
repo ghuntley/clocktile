3290e70d267d58bb1c0874210a0eb8850954e8fc
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-05-18 13:49:37 +1000

    first commit

---
+++ ClockTile.BackgroundTasks/UpdateApplicationTile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.Storage;
+using Windows.UI.Notifications;
+
+namespace ClockTile.BackgroundTasks
+{
+    public sealed class UpdateApplicationTile : IBackgroundTask
+    {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var cancel = new CancellationTokenSource();
+            taskInstance.Canceled += (s, e) =>
+            {
+                cancel.Cancel();
+                cancel.Dispose();
+            };
+
+            var deferral = taskInstance.GetDeferral();
+
+            try
+            {
+                var template = await LoadTemplate("PrimaryTile.xml");
+                var applicationTile = string.Format(template, DateTime.Now.TimeOfDay, DateTime.Now.Date);
+                UpdatePrimaryWith(applicationTile);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                deferral.Complete();
+            }
+        }
+
+        private static async Task<string> LoadTemplate(string fileName)
+        {
+            var folder = await Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            var file = await folder.GetFileAsync(fileName);
+            return await FileIO.ReadTextAsync(file);
+        }
+
+        private static void ResetTileToDefault()
+        {
+            var tileUpdater = TileUpdateManager.CreateTileUpdaterForApplication();
+            tileUpdater.Clear();
+        }
+
+        private static void SetPrimary(XmlDocument xmlDocument)
+        {
+            var tileUpdater = TileUpdateManager.CreateTileUpdaterForApplication();
+
+            // clear everything, display default values from application manifest.
+            tileUpdater.Clear();
+
+            // always display the latest version.
+            tileUpdater.EnableNotificationQueue(false);
+            tileUpdater.EnableNotificationQueueForSquare150x150(false);
+            tileUpdater.EnableNotificationQueueForSquare310x310(false);
+            tileUpdater.EnableNotificationQueueForWide310x150(false);
+
+            tileUpdater.Update(new TileNotification(xmlDocument));
+        }
+
+        private static void UpdatePrimaryWith(string tileXml)
+        {
+            var xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(tileXml);
+
+            SetPrimary(xmlDocument);
+        }
+    }
+}
---
+++ ClockTile/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace ClockTile
+{
+    public sealed partial class MainPage : Page
+    {
+        private readonly ApplicationTrigger _applicationTrigger = new ApplicationTrigger();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public static void UnregisterBackgroundTask(string taskName)
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == taskName)
+                {
+                    task.Value.Unregister(true);
+                }
+            }
+        }
+
+        public async Task UnregisterAndRegisterTask(string taskName, string taskEntryPoint)
+        {
+            UnregisterBackgroundTask(taskName);
+
+            await BackgroundExecutionManager.RequestAccessAsync();
+
+            var builder = new BackgroundTaskBuilder();
+
+            builder.Name = taskName;
+            builder.TaskEntryPoint = taskEntryPoint;
+            builder.SetTrigger(new TimeTrigger(freshnessTime: 15, oneShot: false));
+
+            BackgroundTaskRegistration task = builder.Register();
+
+        }
+
+        public void RunBackgroundTask(string taskName, string taskEntryPoint)
+        {
+            var taskBuilder = new BackgroundTaskBuilder
+            {
+                Name = taskName,
+                TaskEntryPoint = taskEntryPoint,
+                IsNetworkRequested = true
+            };
+
+            try
+            {
+                taskBuilder.SetTrigger(_applicationTrigger);
+                taskBuilder.Register();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            const string taskName = "updateApplicationTile";
+            const string taskEntryPoint = "ClockTile.BackgroundTasks.UpdateApplicationTile";
+
+            Task.Run(async () => await UnregisterAndRegisterTask(taskName, taskEntryPoint));
+            RunBackgroundTask(taskName, taskEntryPoint);
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
